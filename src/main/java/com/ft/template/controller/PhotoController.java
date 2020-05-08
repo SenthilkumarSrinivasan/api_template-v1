8d4e8dca3aeb4f76c4751c9ceb2790352096ee31
Senthilkumar Srinivasan <skumarsrinivasan@bn.com>
2020-05-08 15:02:20 -0400

    minor changes

--- src/main/java/com/ft/template/controller/PhotoController.java
+++ src/main/java/com/ft/template/controller/PhotoController.java
@@ -2,18 +2,14 @@ package com.ft.template.controller;
 
 import com.ft.template.domain.entities.Photo;
 import com.ft.template.service.PhotoService;
-import com.ft.template.service.SampleService;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.Base64;
 
 @Slf4j
 @RestController
